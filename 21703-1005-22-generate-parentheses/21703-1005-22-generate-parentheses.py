b6b570a7ff4ce328936681419acc1ad44ccd3ec1
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-01-15 18:26:27 +0530

    Time: 1 ms, Memory: 18.2 MB - LeetHub

---
+++ 21703-1005-22-generate-parentheses/21703-1005-22-generate-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def recur(close,open,s):
+            if close==0:
+                if open==0:
+                    res.append(s)
+                return 
+            recur(close-1,open+1,s+'(')
+            if open>0:
+                recur(close-1,open-1,s+')') 
+        recur(2*n-1,1,'(')
+        return res
