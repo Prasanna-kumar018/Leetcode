71294cdc252b82b56383469ff84086cb825e28e4
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-01-22 23:55:14 +0530

    Time: 789 ms (6.53%) | Memory: 17.7 MB (60.87%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minSteps(self, n_: int) -> int:
+        INF = 10**20
+        def recur(n,avail):
+            if n==0:
+                return 0
+            if n<0 or avail>n_:
+                return INF
+            best = INF
+            # copy
+            if (n_-n)> avail:
+                best = min(best,recur(n,(n_-n))+1)
+            # paste
+            if avail!=0:
+                best = min(best,recur(n-avail,avail)+1)
+            return best
+        res = recur(n_-1,0)
+        return res
+        
