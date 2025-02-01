db0ce26bb3e952ce51d4d579a49eaedd5e84e4a9
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-02-01 14:39:34 +0530

    Time: 2087 ms (5.04%) | Memory: 30 MB (5.18%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        INF = 10**20
+        n = len(s)
+        # @cache
+        # def recur(idx,isb):
+        #     if idx==n:
+        #         return 0
+        #     res = INF
+        #     res = min(res,recur(idx+1,isb)+1)
+        #     if s[idx]=='a':
+        #         if not isb:
+        #             res = min(res,recur(idx+1,isb))
+        #     else:
+        #         res = min(res,recur(idx+1,True))
+        #     return res
+        # return recur(0,False)
+
+        dp = [[0]*2 for _ in range(n+1)]
+        for idx in range(n-1,-1,-1):
+            for isb in range(2):
+                res = INF
+                res = min(res,dp[idx+1][isb]+1)
+                if s[idx]=='a':
+                    if not isb:
+                        res = min(res,dp[idx+1][isb])
+                else:
+                    res = min(res,dp[idx+1][1])
+                dp[idx][isb]=res
+        return dp[0][0]
