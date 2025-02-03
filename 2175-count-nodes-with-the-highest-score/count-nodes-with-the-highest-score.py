9b5ea3903196a41aed322a9063d0795b01316785
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-02-03 10:57:41 +0530

    Time: 1007 ms (5.22%) | Memory: 56.5 MB (74.07%) - LeetSync

---
+++ 2175-count-nodes-with-the-highest-score/count-nodes-with-the-highest-score.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countHighestScoreNodes(self, parents: List[int]) -> int:
+        n=len(parents)
+        graph = [[] for i in range(n)]
+        for idx,i in enumerate(parents):
+            if i !=-1:
+                graph[idx].append(i)
+                graph[i].append(idx)
+        res = collections.defaultdict(int)
+        def dfs(node,par):
+            nonlocal n,res
+            t = []
+            for x in graph[node]:
+                if x!=par:
+                    t.append(dfs(x,node))
+            s = sum(t)+1
+            t.append(n-s)
+            print(node,t)
+            r = 1
+            for x in t:
+                r *= max(1,x)
+            res[r]+=1
+            return s
+        dfs(0,-1)
+        return res[max(res.keys())]
+        
