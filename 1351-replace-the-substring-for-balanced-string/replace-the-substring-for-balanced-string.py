7880dd20eba365716fe1bc30dec767fe25811d03
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-02-04 08:09:09 +0530

    Time: 328 ms (5.63%) | Memory: 18.1 MB (71.56%) - LeetSync

---
+++ 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.py
@@ -0,0 +1,31 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        n = len(s)
+        d = {'Q':0,'W':0,'E':0,'R':0}
+        need = n//4
+        for c in s:
+            d[c]+=1
+        # for 0
+        res = True
+        for y in d.values():
+            if y!=need:
+                res = False
+        if res: return 0
+        l,r = 0,0
+        def issafe():
+            nonlocal d
+            res = True
+            for x,y in d.items():
+                if y>need:
+                    res = False
+            return res
+        res = 10**20
+        while r<n:
+            d[s[r]]-=1
+            while issafe() and l<=r:
+                print(d,l,r)
+                res = min(res,r-l+1)
+                d[s[l]]+=1
+                l+=1
+            r+=1
+        return res
