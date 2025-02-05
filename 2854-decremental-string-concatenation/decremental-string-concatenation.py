940ec0bf01ad9337c60323438389d4ef488fcc14
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-02-05 11:31:41 +0530

    Time: 336 ms (33.33%) | Memory: 54.5 MB (16.00%) - LeetSync

--- 2854-decremental-string-concatenation/decremental-string-concatenation.py
+++ 2854-decremental-string-concatenation/decremental-string-concatenation.py
@@ -6,18 +6,18 @@ class Solution:
         for idx,x in enumerate(words):
             length[idx]=len(x)
         @cache
-        def recur(idx,first,last):
+        def recur(idx,f,l):
             if idx==n:
                 return 0
             res = INF
             val = length[idx]
-            if last==words[idx][0]:
-                res=min(res,recur(idx+1,first,words[idx][-1])+val-1)
+            if f ==words[idx][-1]:
+                res = min(res,recur(idx+1,words[idx][0],l)+(val-1))
             else:
-                res=min(res,recur(idx+1,first,words[idx][-1])+val)
-            if first==words[idx][-1]:
-                res=min(res,recur(idx+1,words[idx][0],last)+val-1)
+                res = min(res,recur(idx+1,words[idx][0],l)+val)
+            if l ==words[idx][0]:
+                res = min(res,recur(idx+1,f,words[idx][-1])+(val-1))
             else:
-                res=min(res,recur(idx+1,words[idx][0],last)+val)
-            return res
+                res = min(res,recur(idx+1,f,words[idx][-1])+val)
+            return  res
         return recur(1,words[0][0],words[0][-1])+length[0]
