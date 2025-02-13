41fe451c258c1dd217ecf1cbf15df295663677e2
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-02-13 18:41:45 +0530

    Time: 103 ms (23.08%) | Memory: 40.6 MB (10.77%) - LeetSync

---
+++ 3166-minimum-number-of-groups-to-create-a-valid-assignment/minimum-number-of-groups-to-create-a-valid-assignment.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minGroupsForValidAssignment(self, balls: List[int]) -> int:
+        d=collections.Counter(balls)
+        INF = 10**10
+        def good(i):
+
+            for x in d.values():
+                l = x//i 
+                g = x % i # if g > l then it wont be divided into target and
+                # target+1 .Here i is the target
+                if g>l:
+                    return False
+            return True
+        def calc(i):
+            group = 0
+            for x in d.values():
+                g = x // i # combination of target and target+1
+
+                l2 = x%(i+1)
+                g2 = x//(i+1)
+
+                # if l2>g2 and l2!=i:
+                #     g2= INF
+                if l2>0:
+                    g2+=1
+                group+= min(g,g2)
+            return group
+        mini = min(d.values())
+        best = INF
+        for i in range(1,mini+1):
+            if good(i):
+                best = min(best,calc(i))
+        return best
