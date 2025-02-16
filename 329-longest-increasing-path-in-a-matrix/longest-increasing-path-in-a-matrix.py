fe9cffcedf83b2c827c2a2bc9504181addae0e0c
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-02-16 14:27:27 +0530

    Time: 352 ms (5.09%) | Memory: 53.3 MB (5.01%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        m = len(matrix)
+        n = len(matrix[0])
+        dir = [(-1,0),(1,0),(0,1),(0,-1)]
+        @cache
+        def go(x,y,parx,pary):
+            res = 0
+            for dx,dy in dir:
+                nx,ny = x+dx,y+dy
+                if 0<= nx < m and 0<= ny < n and matrix[nx][ny]>matrix[x][y]:
+                    res = max(res,go(nx,ny,x,y)+1)
+            return res
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                ans = max(ans,go(i,j,-1,-1)+1)
+        return ans
