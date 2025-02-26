60bb5ef016846fa7a6768106042056294f2196a0
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-02-26 16:02:52 +0530

    Time: 1602 ms (21.72%) | Memory: 36.1 MB (66.57%) - LeetSync

---
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minimumTime(self, grid: List[List[int]]) -> int:
+        q = []
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if (grid[i][j]%2) != ((i+j)%2):
+                    grid[i][j]+=1
+        if grid[0][1]>1 and grid[1][0]>1:
+            return -1
+        def push(val):
+            heapq.heappush(q,val)
+        def pop():
+            return heapq.heappop(q)
+        push((0,0,0)) # time,x,y
+        INF = 10**20
+        dir = [(-1,0),(1,0),(0,1),(0,-1)]
+        d = collections.defaultdict(lambda : collections.defaultdict(lambda : INF))
+        d[0][0]=0
+        while q:
+            time,x,y = pop()
+            if d[x][y] < time:
+                continue
+            for dx,dy in dir:
+                nx,ny = x+dx,y+dy
+                if 0<= nx < m and 0<= ny<n:
+                    nd = max(time+1,grid[nx][ny])
+                    if d[nx][ny]>nd:
+                        d[nx][ny]=nd
+                        push((d[nx][ny],nx,ny))
+            # print(q)
+        return d[m-1][n-1]
