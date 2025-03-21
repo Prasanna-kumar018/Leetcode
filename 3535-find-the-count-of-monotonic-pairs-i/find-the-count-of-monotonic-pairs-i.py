bf494660db855f2ce2b3f157d3429e96381a1ef6
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-03-21 19:08:08 +0530

    Time: 3314 ms (16.22%) | Memory: 18.1 MB (81.08%) - LeetSync

--- 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.py
+++ 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.py
@@ -2,17 +2,40 @@ class Solution:
     def countOfPairs(self, nums: List[int]) -> int:
         mod = 10**9+7
         n = len(nums)
-        @cache
-        def recur(idx,prev):
-            nonlocal n
-            if idx==n:
-                return 1
-            total = 0
-            for i in range(nums[idx]+1):
-                if  prev <= i and (nums[idx-1]-prev)>=(nums[idx]-i):
-                    total+=recur(idx+1,i)
-            return total%mod
-        total = 0
-        for i in range(nums[0]+1):
-            total+=recur(1,i)
-        return total%mod
+        # @cache
+        # def recur(idx,prev):
+        #     nonlocal n
+        #     if idx==n:
+        #         return 1
+        #     total = 0
+        #     for i in range(nums[idx]+1):
+        #         if  prev <= i and (nums[idx-1]-prev)>=(nums[idx]-i):
+        #             total+=recur(idx+1,i)
+        #     return total%mod
+        # total = 0
+        # for i in range(nums[0]+1):
+        #     total+=recur(1,i)
+        # return total%mod
+        M = max(nums)
+        dp = [0]*(M+1)
+        for i in range(M+1):
+            dp[i]=1
+        ans = sum(dp)
+        for idx in range(n-1,-1,-1):
+            """
+            prev<= i and i>=(nums[idx]-nums[idx-1] + prev) 
+            """
+            # prefix = 
+            res = list(dp)
+            for prev in range(M+1):
+                total = 0
+                for i in range(nums[idx]+1):
+                    if  prev <= i and (nums[idx-1]-prev)>=(nums[idx]-i):
+                        total+=dp[i]
+                res[prev]=total
+            dp = res
+            if idx==1:
+                print(res)
+                ans = sum(res)
+                break
+        return ans%mod
