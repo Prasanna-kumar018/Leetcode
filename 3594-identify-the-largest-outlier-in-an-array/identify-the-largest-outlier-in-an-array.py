cefcc5a6ec2ca31dc787b54a52aa711ec7cff339
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-03-31 08:26:18 +0530

    Time: 142 ms (79.26%) | Memory: 28.3 MB (61.31%) - LeetSync

---
+++ 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getLargestOutlier(self, nums: List[int]) -> int:
+        total = sum(nums)
+        count = Counter(nums)
+        """
+        total = 2* sum() + outlier
+        """
+        res = -10**20
+        for x in count.keys(): # 1000 * 1000
+            out = total - 2*x
+            count[x]-=1
+            if count[out]>=1:
+                res = max(out,res)
+            count[x]+=1
+        return res
