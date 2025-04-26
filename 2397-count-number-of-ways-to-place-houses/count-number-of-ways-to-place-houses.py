4308fe5aedf6e317e82867fdba3913f100d54f45
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-04-26 12:19:35 +0530

    Time: 1075 ms (5.37%) | Memory: 65.5 MB (12.20%) - LeetSync

---
+++ 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countHousePlacements(self, n: int) -> int:
+        MOD = 10**9+7
+        @cache
+        def recur(prev1,prev2,n):
+            if n==0:
+                return 1
+            res = 0
+            for x in [True,False]:
+                for y in [True,False]:
+                    if x or y:
+                        if x and not prev1 and y and not prev2:
+                            res+=recur(x,y,n-1)
+                    else:
+                        res += recur(x,y,n-1)
+            return res
+        x =recur(False,False,n)
+        return (x*x)%MOD
