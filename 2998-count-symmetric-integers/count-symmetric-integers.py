599623c77ddadb3488bcc96b4a069c9612287e25
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-05-16 09:35:51 +0530

    Time: 51 ms (96.64%) | Memory: 18.3 MB (11.19%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.py
+++ 2998-count-symmetric-integers/count-symmetric-integers.py
@@ -1,9 +1,57 @@
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
-        res = 0
-        for x in range(low,high+1):
-            x = list(map(int,str(x)))
-            n = len(x)
-            if n%2==0:
-                res += 1 if (sum(x[:n//2])==sum(x[-(n//2):])) else 0
-        return res
+        def go(v):
+            if v%2==1:
+                return 0
+            aa = v//2
+            @cache
+            def help(idx,s):
+                nonlocal aa
+                if idx==0:
+                    if s==0:
+                        return 1
+                    return 0
+                res  =0 
+                print(idx,s)
+                for i in range(10):
+                    val = -i if idx<=aa else i
+                    res += help(idx-1,s+val)
+                return res
+            t = 0
+            for i in range(1,10):
+                t += help(v-1,i)
+            help.cache_clear()
+            return t
+
+        def get(n,ee):
+            aa = n//2
+            @cache
+            def help2(idx,s,isless):
+                nonlocal ee,aa,n
+                if idx==n:
+                    if s==0:
+                        return 1
+                    return 0
+                v = int(ee[idx])
+                start =1  if idx==0 else 0
+                res = 0
+                for i in range(start,10):
+                    val = i if idx<aa else -i
+                    if isless:
+                        res += help2(idx+1,s+val,isless)
+                    elif i<=v:
+                        f = True if i<v else False
+                        res += help2(idx+1,s+val,f)
+                return res
+            x = help2(0,0,False)
+            help2.cache_clear()
+            return x
+        def f(val):
+            val = str(val)
+            x = len(val)
+            total = 0
+            for i in range(2,x):
+                total += go(i)
+            total+=get(x,val)
+            return total
+        return f(high)-f(low-1)
