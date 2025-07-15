85849dce9651db9e4a184ce5755260ff257aad2e
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-07-15 22:48:01 +0530

    Time: 2267 ms (5.26%) | Memory: 463.8 MB (5.26%) - LeetSync

---
+++ 2601-number-of-great-partitions/number-of-great-partitions.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countPartitions(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        pre =[0]
+        MOD = 10**9+7
+        for i in range(n):
+            pre.append(pre[-1]+nums[i])
+        @cache
+        def recur(idx,left):
+            right = pre[idx]-left
+            if idx==n:
+                if left==k and right>=k:
+                    return 1
+                return 0
+            if left==k and right>=k:
+                return pow(2,n-idx,MOD)
+            total = 0
+            # left
+            if left+nums[idx]>=k:
+                total += recur(idx+1,min(k,right))
+            else:
+                total += recur(idx+1,min(k,left+nums[idx]))
+            # right     
+            # if right+nums[idx]>=k:
+            #     total+= recur(idx+1,left)
+            # else:
+            #     total += recur(idx+1,left)
+            total+= recur(idx+1,min(k,left))
+            return total%MOD
+    
+        return recur(0,0)
+
