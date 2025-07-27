18ec26cab01ef736a587a708ab13164d27fb3eb1
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-07-27 11:47:19 +0530

    Time: 769 ms (42.90%) | Memory: 120.4 MB (36.76%) - LeetSync

---
+++ 3863-power-grid-maintenance/power-grid-maintenance.py
@@ -0,0 +1,49 @@
+class Solution:
+    def processQueries(self, c: int, connections: List[List[int]], queries: List[List[int]]) -> List[int]:
+
+        blocked = collections.Counter()
+        for x,y in queries:
+            if x==2:
+                blocked[y]+=1
+        par = {}
+        mini = {}
+        INF = 10**20
+        def find(x):
+            if x not in par:
+                par[x]=x
+                mini[x]=(x if blocked[x]==0 else INF)
+            if par[x]!=x:
+                par[x]=find(par[x])
+            return par[x]
+
+        def union(x,y):
+            # print(x,y)
+            x= find(x)
+            y= find(y)
+            if x==y:
+                return
+            par[x]=y
+            mini[y]=min(mini[y],mini[x])
+
+
+        g = collections.defaultdict(list)
+        for x,y in connections:
+            g[x].append(y)
+            g[y].append(x)
+            union(x,y)
+        # print()
+        res = []
+        for x,y in reversed(queries):
+            if x==1:
+                if blocked[y]==0:
+                    res.append(y)
+                else:
+                    MAXI = mini[find(y)]
+                    res.append(MAXI if MAXI != INF else -1)
+            else:
+                blocked[y]-=1
+                if blocked[y]==0:
+                    p = find(y)
+                    mini[p]=min(mini[p],y)
+        res.reverse()
+        return res
