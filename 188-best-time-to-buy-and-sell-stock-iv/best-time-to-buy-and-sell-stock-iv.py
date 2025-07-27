0ee939ffd63e3766c7baa66b4ced097fabff631b
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-07-28 03:21:34 +0530

    Time: 75 ms (48.78%) | Memory: 18 MB (63.75%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -5,9 +5,8 @@ class Solution:
         sell = [0]*k
         for val in prices:
             for i in range(k):
-                if i==0:
-                    buy[0]=max(buy[0],-val)
-                if i!=0:
+                buy[0]=max(buy[0],-val)
+                if i>0:
                     buy[i]=max(buy[i],sell[i-1]-val)
                 sell[i]=max(sell[i],buy[i]+val)
         return sell[-1]
