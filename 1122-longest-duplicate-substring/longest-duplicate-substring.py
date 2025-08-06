9cb702e828e6506138fc0e199b1cd228c502df18
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-08-06 11:26:32 +0530

    Time: 886 ms (86.50%) | Memory: 23.5 MB (76.17%) - LeetSync

---
+++ 1122-longest-duplicate-substring/longest-duplicate-substring.py
@@ -0,0 +1,39 @@
+class Solution:
+    def longestDupSubstring(self, s: str) -> str:
+        mod=1e12+7
+        n=len(s)
+        def isPossible(len_):
+            l=0
+            r=len_-1
+            value = 0
+            for i in range(l,r+1):
+                asc= ord(s[i])
+                value =  (value *26 + asc)%mod
+            d={}
+            d[value]=0
+            while r+1 <n:
+                r+=1
+                asc= ord(s[l])
+                value = (((value-(power[len_-1]*asc)%mod)*26)%mod+ (ord(s[r])))%mod
+                l+=1
+                if value in d:
+                    return d[value]
+                else:
+                    d[value]=l
+            return -1
+        power=[0]*n
+        power[0]=1
+        for i in range(1,n):
+            power[i]=(power[i-1]*26)%mod
+        l,r=1,n-1
+        ans =""
+        while l <= r:
+            mid= (l+r)//2
+            x=isPossible(mid)
+            if  x!=-1:
+                l = mid+1
+                if len(ans) < len(s[x:(x+mid)]):
+                    ans=s[x:(x+mid)]
+            else:
+                r=mid-1
+        return ans
