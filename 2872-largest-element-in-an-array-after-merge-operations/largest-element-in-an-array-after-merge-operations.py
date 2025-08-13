5dfa93ca64e43ec1ca54fdf996e554b4133ee84a
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-08-13 12:57:11 +0530

    Time: 83 ms (36.90%) | Memory: 33.7 MB (21.92%) - LeetSync

--- 2872-largest-element-in-an-array-after-merge-operations/largest-element-in-an-array-after-merge-operations.py
+++ 2872-largest-element-in-an-array-after-merge-operations/largest-element-in-an-array-after-merge-operations.py
@@ -4,11 +4,17 @@ class Solution:
         N = len(nums)
         INF = 10**20
         last = -INF
+        """
+        this works because
+        if we get some x that is greater than the current sum i.e. last
+        _ _ _ _ _ _ x last _ _ _ 
+        if x>last:
+            then x will grow again which will never be <= last
+        """
         for i in range(N-1,-1,-1):
             if nums[i]<=last:
                 last += nums[i]
             else:
                 last = nums[i]
-
             best = max(best,last)
         return best
