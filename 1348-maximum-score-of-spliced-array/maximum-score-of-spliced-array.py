2cedbe3701bf1849410cd63de79c6802476f3f79
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-08-14 23:06:13 +0530

    Time: 1747 ms (5.19%) | Memory: 641.5 MB (5.19%) - LeetSync

--- 1348-maximum-score-of-spliced-array/maximum-score-of-spliced-array.py
+++ 1348-maximum-score-of-spliced-array/maximum-score-of-spliced-array.py
@@ -3,13 +3,13 @@ class Solution:
         n = len(nums1)
         INF = -10**20
         @cache
-        def recur(idx,takeone,k):
+        def recur(idx,lasttakeone,k):
             if k>2:
                 return INF
             if idx==n:
                 return 0
             res = INF
-            if takeone:
+            if lasttakeone:
                 res = max(res,nums1[idx]+recur(idx+1,True,k))
                 res = max(res,nums2[idx]+recur(idx+1,False,k+1))
             else:
