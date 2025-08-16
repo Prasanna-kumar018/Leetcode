fb63ab36d07f2fd714036dae9a176503cb3c3128
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-08-16 14:57:39 +0530

    Time: 367 ms (9.09%) | Memory: 33.2 MB (16.97%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.py
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.py
@@ -9,15 +9,15 @@ class Solution:
         n = len(weights)
         mitotal,matotal = 0,0
         for i in range(n-1):
-            push(mini,weights[i]+weights[i+1])
-            push(maxi,-(weights[i]+weights[i+1]))
+            push(mini,-(weights[i]+weights[i+1]))
+            push(maxi,(weights[i]+weights[i+1]))
             mitotal+=(weights[i]+weights[i+1])
             matotal+=(weights[i]+weights[i+1])
             if len(mini)==k:
                 x = pop(mini)
-                mitotal-=x
+                mitotal-=abs(x)
             if len(maxi) == k:
                 x = pop(maxi)
                 matotal-=abs(x)
-        return mitotal-matotal
+        return matotal-mitotal
             
