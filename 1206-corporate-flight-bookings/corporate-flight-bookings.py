1fd5550afc463c3321f4638cc62cbcb6cdf55070
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-08-17 22:52:28 +0530

    Time: 705 ms (5.06%) | Memory: 29.4 MB (7.80%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.py
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.py
@@ -4,7 +4,6 @@ class Solution:
         We can do range addition using sweep line very easily but we are going to 
         try this range addition in fenwick tree
 
-        """
         arr = [0]*(n+1)
         for x,y,z in bookings:
             arr[x]+=z
@@ -13,3 +12,36 @@ class Solution:
         for i in range(1,n+1):
             arr[i]+=arr[i-1]
         return arr[1:]
+
+        """
+
+        tree1 = [0]*(n+1)
+        tree2 = [0]*(n+1)
+        def update(tree,idx,val):
+            while idx<=n:
+                tree[idx]+=val
+                idx += (idx&(-idx))
+        def updateit(x,y,z):
+            update(tree1,x,z)
+            update(tree1,y+1,-z)
+            update(tree2,x,-(x-1)*z)
+            update(tree2,y+1,y*z)
+        def get(tree,idx):
+            s = 0
+            while idx>0:
+                s += tree[idx]
+                idx -= (idx&(-idx))
+            return s
+        def gett(idx):
+            return get(tree1,idx)*idx + get(tree2,idx)
+
+        def getit(l,r):
+            return gett(r)-gett(l-1) 
+        for x,y,z in bookings:
+            updateit(x,y,z)
+        
+        res = []
+        for i in range(1,n+1):
+            res.append(getit(i,i))
+        return res
+        
