e38226068ead5f8ea96f691b41ee0b2ab3a646f9
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-08-25 00:47:07 +0530

    Time: 1298 ms (52.06%) | Memory: 34.4 MB (79.03%) - LeetSync

---
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestValidSubstring(self, word: str, forbidden: List[str]) -> int:
+        f = set(forbidden)
+        N = len(word)
+        MAXI = 10
+        def bad():
+            M = len(curr)
+            w = ""
+            for i in range(M-1,-1,-1):
+                if M-i>10:
+                    break
+                w = curr[i] + w
+                if w in f:
+                    return True
+            return False
+        ans = 0
+        curr = collections.deque()
+        for r in range(N):
+            curr.append(word[r])
+            while bad():
+                curr.popleft()
+            ans = max(ans,len(curr))
+        return ans
