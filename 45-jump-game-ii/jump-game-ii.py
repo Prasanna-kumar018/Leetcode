569f3dc73df08353ccddf14c0accd63157af750a
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-08-26 16:11:48 +0530

    Time: 1 ms (98.24%) | Memory: 18.8 MB (20.75%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # GREEDY
+        i = 0
+        n = len(nums)
+        if n==1:
+            return 0
+        count=0
+        while i<n:
+            maxi = nums[i]+i
+            des = nums[i]+i
+            next = -1
+            while i<=min(des,n-1):
+                if i+nums[i]>maxi:
+                    maxi=i+nums[i]
+                    next=i
+                i+=1
+            count+=1
+            if des>=n-1:
+                return count
+            if next==-1:
+                return -1
+            i = next
+        return count
