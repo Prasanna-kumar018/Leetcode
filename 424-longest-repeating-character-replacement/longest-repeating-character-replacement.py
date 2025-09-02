108401e8a900d6f7be9229f3e442dc8105c3c640
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-09-02 23:36:51 +0530

    Time: 183 ms (22.55%) | Memory: 18.2 MB (23.97%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,33 +1,13 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        ans = -1
-        n = len(s)
-        left ,right = 0,n
-        def isSafe(idx):
-            if idx==0:
-                return True
-            d=collections.defaultdict(int)
-            l,r=0,0
-            while r<idx:
-                d[s[r]]+=1
-                r+=1
-            while r<=n:
-                val = max(d.values())
-                # print("sdf",val)
-                if val+k >=idx:
-                    return True
-                if r<n:
-                    d[s[r]]=d.get(s[r],0)+1
-                d[s[l]]=d.get(s[l],0)-1
-                l+=1
-                r+=1
-            return False
-        while left<=right:
-            mid = (left+right)//2 
-            if isSafe(mid):
-                left=mid+1
-                ans=mid
-            else:
-                right=mid-1
-        return ans
         
+        n = len(s)
+        l = res = 0
+        d = collections.defaultdict(int)
+        for r in range(n):
+            d[s[r]]+=1
+            while l<=r and (r-l+1)>(max(d.values())+k):
+                d[s[l]]-=1
+                l+=1
+            res = max(res,r-l+1)
+        return  res
