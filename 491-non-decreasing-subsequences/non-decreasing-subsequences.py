e8ab233d2f75c0e41bac0536aa3f2ae811a53a96
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-09-02 22:50:08 +0530

    Time: 14 ms (95.52%) | Memory: 23.7 MB (82.78%) - LeetSync

---
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        def recur(prev,l):
+            nonlocal res
+            vis = set()
+            for idx in range(prev+1,n):
+                if nums[idx] in vis:
+                    continue
+                if nums[idx] >= nums[prev]:
+                    l.append(nums[idx])
+                    recur(idx,l)
+                    l.pop()
+                vis.add(nums[idx])
+            if len(l)>=2:
+                res.append(list(l))
+        vv = set()
+        for i in range(n):
+            if nums[i] in vv:
+                continue
+            recur(i,[nums[i]])
+            vv.add(nums[i])
+        return res
