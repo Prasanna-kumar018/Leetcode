8105117124d8e49525ea1361e9d8d93a3a494977
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-09-03 14:43:44 +0530

    Time: 2299 ms (28.11%) | Memory: 465.3 MB (5.11%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.py
+++ 2808-painting-the-walls/painting-the-walls.py
@@ -2,17 +2,19 @@ class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
         INF = 10**20
         N = len(cost)
-        lower = -N
-        upper = N
         @cache
         def recur(idx,have):
+            if have>=N:
+                return 0
             if idx==N:
-                if have>=0:
-                    return 0
                 return INF
             res = INF
-            res = min(res,recur(idx+1,max(lower,have-1)))
-            res = min(res,recur(idx+1,min(have+time[idx],upper))+cost[idx])
+            res = min(res,recur(idx+1,have))
+            """
+            time[idx] --> no of free painters
+            1 --> for the current painter
+            """
+            res = min(res,recur(idx+1,min(have+time[idx]+1,N))+cost[idx])
             return res
         x = recur(0,0)
         return x 
