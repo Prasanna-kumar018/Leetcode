b746c2a467242798e20ce9a2457eff44a4ff9402
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-09-03 10:54:17 +0530

    Time: 3 ms (53.89%) | Memory: 18.1 MB (94.23%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,31 +1,21 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        g = collections.defaultdict(list)
-        n = 0
+        par = {}
+        def find(x):
+            if x not in par:
+                par[x]=x
+            if par[x]!=x:
+                par[x]=find(par[x])
+            return par[x]
+        def union(a,b):
+            a = find(a)
+            b = find(b)
+            if a==b:
+                return True
+            par[a]=b
+            return False
+        
         for x,y in edges:
-            g[x].append(y)
-            g[y].append(x)
-        INF = 10**20
-        lt = collections.defaultdict(lambda :INF)
-        idx = 0
-        res = set()
-        def arti(start,par):
-            nonlocal idx,res
-            lt[start]=idx
-            idx+=1
-            for neigh in g[start]:
-                if neigh==par:
-                    continue
-                if lt[neigh]==INF:
-                    arti(neigh,start)
-                    if lt[neigh]<=lt[start]:
-                        lt[start]=lt[neigh]
-                        res.add((start,neigh)) 
-                elif lt[neigh]<lt[start]:
-                    lt[start]=lt[neigh]
-                    res.add((start,neigh)) 
-        arti(1,-1)
-        for x,y in edges[::-1]:
-            if (x,y) in res or (y,x) in res:
+            if union(x,y):
                 return [x,y]
-        return [] # dummy
+        return [-1,-1]
