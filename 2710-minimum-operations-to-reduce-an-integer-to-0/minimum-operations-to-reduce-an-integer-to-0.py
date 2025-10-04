5de10a643dd006ad13fc494cc5ddeaa27e21fde6
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-10-04 11:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (19.96%) - LeetSync

---
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, n: int) -> int:
+        """
+        2^3 - 1 = 2^2 + 2^1 + 2^0
+        8   - 1 = (4 + 2 + 1) 
+        """
+        res = 0
+        while n>0:
+            if (n &3 == 3):
+                n+=1
+                res +=1
+            else:
+                res += (n&1)
+                n>>=1
+        return res
