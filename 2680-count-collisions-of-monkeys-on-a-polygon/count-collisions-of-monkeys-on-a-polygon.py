ce983eaca077dec14ab9154bb9b43c4b1e2a2294
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-12-01 21:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (47.77%) - LeetSync

---
+++ 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
@@ -0,0 +1,12 @@
+class Solution:
+    def monkeyMove(self, n: int) -> int:
+        MOD = 10**9 + 7
+        @cache
+        def p(left):
+            if left==0:
+                return 1
+            x = p(left//2)
+            if left%2==0:
+                return (x*x) % MOD
+            return (x*x*2) % MOD
+        return ( p(n) - 2) % MOD
