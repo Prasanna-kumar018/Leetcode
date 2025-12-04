8746806f6073fda31879fd2f80b4dd3a3b6bd111
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-12-04 22:12:27 +0530

    Time: 29 ms (5.71%) | Memory: 30 MB (61.90%) - LeetSync

---
+++ 2621-find-xor-beauty-of-array/find-xor-beauty-of-array.py
@@ -0,0 +1,41 @@
+class Solution:
+    def xorBeauty(self, nums: List[int]) -> int:
+        """
+        - (0,0,0) with effective value ((1 | 1) & 1) = 1
+        - (0,0,1) with effective value ((1 | 1) & 4) = 0
+        - (1,1,0) with effective value ((4 | 4) & 1) = 0
+        - (1,1,1) with effective value ((4 | 4) & 4) = 4
+
+        - (0,1,0) with effective value ((1 | 4) & 1) = 1
+        - (1,0,0) with effective value ((4 | 1) & 1) = 1
+        - (0,1,1) with effective value ((1 | 4) & 4) = 4
+        - (1,0,1) with effective value ((4 | 1) & 4) = 4
+
+
+        0 1 2 3
+
+        0 0 0
+        0 0 1
+        0 0 2
+        0 0 3
+        1 1 0
+        1 1 1
+        1 1 2
+        1 1 3
+        2 2 0
+        2 2 1
+        2 2 2
+        2 2 3
+        3 3 0
+        3 3 1
+        3 3 2
+        3 3 3 
+
+        """
+        n = len(nums)
+        res = v = 0
+        for i in range(n):
+            v ^= (nums[i]|nums[i])
+        for j in range(n):
+            res ^= (v & nums[j])
+        return res
