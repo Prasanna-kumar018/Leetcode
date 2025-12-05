787c757701fdf7f7ab9804f6acd4b19b9be48a96
Prasanna-kumar018 <127648148+Prasanna-kumar018@users.noreply.github.com>
2025-12-05 13:43:57 +0530

    Time: 4 ms (46.76%) | Memory: 18 MB (30.22%) - LeetSync

--- 2595-smallest-value-after-replacing-with-sum-of-prime-factors/smallest-value-after-replacing-with-sum-of-prime-factors.py
+++ 2595-smallest-value-after-replacing-with-sum-of-prime-factors/smallest-value-after-replacing-with-sum-of-prime-factors.py
@@ -1,19 +1,17 @@
 class Solution:
     def smallestValue(self, n: int) -> int:
-        MAXI = max(2,n+1)
-        res = [[] for _ in range(MAXI)]
-        for i in range(2,MAXI):
-            j = i
-            while j<MAXI:
-                res[j].append(i)
-                j+=i
         def get(x):
-            v = x
+            i = 2
             s = 0
-            for a in res[v]:
-                while x%a==0:
-                    s += a
-                    x //= a 
+            v = x
+            while i*i<=v:
+                while x%i==0:
+                # Because of this Loop all the Prime factors will only become as i
+                    s += i
+                    x //= i
+                i+=1
+            if x>1: # This will be Prime For sure ....
+                s += x
             return s
 
         while n!=get(n):
